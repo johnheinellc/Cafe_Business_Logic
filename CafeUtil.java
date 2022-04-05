983fef115d71d8bdcd67481b9e64224a7671c035
johnheinellc <johnheinellc@gmail.com>
2022-04-05 16:36:44 -0400

    first commit

---
+++ CafeUtil.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class CafeUtil{
+
+// 1st
+    // public int getStreakGoal(){
+    // int totalVisit =0;
+    //     for(int visit=0; visit<=10; visit++){
+    //         // System.out.println(visit);
+    //         totalVisit += visit;
+    //     }
+    //     return totalVisit;
+    // }
+
+// 2nd
+    // public double getOrderTotal(double[] prices){
+    // double totalPrice = 0;
+    //     for(int price=0; price<prices.length; price++){
+    //         // System.out.println(prices[price]);
+    //         totalPrice += prices[price];
+    //     }
+    //     return totalPrice;
+    // }
+
+// 3rd
+    // public void displayMenu(ArrayList<String> menu){
+    // // String totalMenu = 0;
+    // for(int i=0; i<menu.size(); i++){
+    //     System.out.println(i + " " + menu.get(i));
+    // }}
+
+// 4th
+    // public void addCustomer(ArrayList<String> customers){
+    //     System.out.println("Enter name here");
+    //     String userName = System.console().readLine();
+    //     // System.out.println(userName);
+    //     customers.add(userName);
+    //     System.out.println("Hello " + userName);
+    //     System.out.println("There are " + customers.size() + " in front of you.");
+    // }
+
+}
---
+++ TestCafe.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class TestCafe {
+    public static void main(String[] args) {
+        CafeUtil Cafe_Business_Logic = new CafeUtil();
+    
+// 1st
+        // System.out.println("\n----- Streak Goal Test -----");
+        // System.out.printf("Purchases needed by week 10: %s \n\n", Cafe_Business_Logic.getStreakGoal());
+    
+// 2nd
+        // System.out.println("----- Order Total Test-----");
+        // double[] lineItems = {3.5, 1.5, 4.0, 4.5};
+        // System.out.printf("Order total: %s \n\n",Cafe_Business_Logic.getOrderTotal(lineItems));
+    
+// 3rd
+        // System.out.println("----- Display Menu Test-----");
+        // ArrayList<String> menu = new ArrayList<String>();
+        // menu.add("drip coffee");
+        // menu.add("cappuccino");
+        // menu.add("latte");
+        // menu.add("mocha");
+        // Cafe_Business_Logic.displayMenu(menu);
+    
+// 4th
+        // System.out.println("\n----- Add Customer Test-----");
+        // ArrayList<String> customers = new ArrayList<String>();
+        // // --- Test 4 times ---
+        // for (int i = 0; i < 4; i++) {
+        //     Cafe_Business_Logic.addCustomer(customers);
+        //     System.out.println("\n");
+        // }
+
+    }
+}
+
